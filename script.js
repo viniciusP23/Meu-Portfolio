6e39ad9becfeabe08f726b24c703c0b45dfd7539
viniciusP23 <vinicius_pereira1@live.com>
2025-10-15 17:41:52 -0300

    Meu Portfolio

---
+++ script.js
@@ -0,0 +1,79 @@
+
+// ENVIO PARA MEU WHATSAPP               
+function enviarWhats(event) {
+    const nome = document.getElementById("nome").value
+    const mensagem = document.getElementById("mensagem").value
+    const telefone = "5514998871712"
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    window.open(url, "_blank")
+
+    event.preventDefault()
+}
+
+
+// BARRA DE NAVEGAÇÃO
+let list = document.querySelectorAll(".navigation li")
+
+function activeLink() {
+    list.forEach((item) => {
+        item.classList.remove("active")
+        this.classList.add("active")
+    })
+}
+
+list .forEach((item) => {
+    item.addEventListener("click", activeLink)
+})
+
+
+// CARROSSEL
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let active = 4; 
+
+function loadShow() {
+    let stt = 0;
+
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+
+    for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+
+    stt = 0;
+    for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+
+loadShow();
+
+next.onclick = function () {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+};
+
+prev.onclick = function () {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+};
+
+
